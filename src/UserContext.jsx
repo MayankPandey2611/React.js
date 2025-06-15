4c284c22f292eb5b4478f7e757a04ac8375a7a89
MayankPandey2611 <kitupnd@gmail.com>
2025-06-15 16:42:22 +0530

    changes

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -1,17 +1,17 @@
-import { useState , createContext } from "react";
+import { useState, createContext } from "react";
 
-let Mycontext = createContext();
-const UserContext  = ({children})=>{
-const [name,setname] = useState("mayank !!");
-    return(
-        <>
-<Mycontext.Provider value={{name}}>
-    {children}
-    <h3>using children tag in UserContext code</h3>
-</Mycontext.Provider>
-        </>
-    )
-}
+
+const Mycontext = createContext();
+
+const UserContext = ({ children }) => {
+  const [name, setName] = useState("mayank !!");
+
+  return (
+    <Mycontext.Provider value={{ name, setName }}>
+      {children}
+    </Mycontext.Provider>
+  );
+};
 
 export default UserContext;
-export  {Mycontext};
+export { Mycontext };
